16c25ba34dfa997734ac34a2843bb06b7ffa97e2
Ömer Sinan Özer <80464808+Vergilerindir@users.noreply.github.com>
2023-10-23 23:38:10 +0300

    Update main.c
    1000

--- fastblinktrio/Core/Src/main.c
+++ fastblinktrio/Core/Src/main.c
@@ -19,7 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "string.h"
-#define zaman 33
+#define zaman 1000
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
