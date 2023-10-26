e257143a36cd0af4eb5b06b4320d85a4ffaa6810
Ömer Sinan Özer <80464808+Vergilerindir@users.noreply.github.com>
2023-10-26 14:07:07 +0300

    Update main.c

--- fastblinktrio/Core/Src/main.c
+++ fastblinktrio/Core/Src/main.c
@@ -19,7 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "string.h"
-const int SABIT = 70;
+const int SABIT = 50;
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
